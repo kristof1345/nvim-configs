3104677d2936b6a0b740ae4c1b1327a7c3865006
kristof1345 <kristofnemeth1345@gmail.com>
2024-09-13 22:38:11 +0200

    ahhh

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,6 +1,6 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
@@ -10,33 +10,33 @@ M.ui = {
 }
 
 M.base46 = {
-	theme = "catppuccin",
+  theme = "catppuccin",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 M.nvimtree = {
   git = {
-    enable = true,  -- Enable Git integration
-    ignore = false,  -- Show ignored files
+    enable = true, -- Enable Git integration
+    ignore = false, -- Show ignored files
   },
   renderer = {
-    highlight_git = true,  -- Highlight files based on Git status
+    highlight_git = true, -- Highlight files based on Git status
     icons = {
       show = {
-        git = true,  -- Show Git icons
-        folder = true,  -- Show folder icons
-        file = true,  -- Show file icons
+        git = true, -- Show Git icons
+        folder = true, -- Show folder icons
+        file = true, -- Show file icons
       },
     },
   },
   view = {
-    side = "left",  -- Position of nvim-tree (left or right)
-    width = 30,  -- Width of the tree
-    auto_resize = true,  -- Automatically resize the tree
+    side = "left", -- Position of nvim-tree (left or right)
+    width = 30, -- Width of the tree
+    auto_resize = true, -- Automatically resize the tree
   },
 }
 
